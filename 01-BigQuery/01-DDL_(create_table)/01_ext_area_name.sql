903966b52268bbf22ee44331fab969409e57dbe8
daac9999 <51725221+daac9999@users.noreply.github.com>
2021-05-24 19:58:40 -0300

    Offline_files_added

---
+++ 01-BigQuery/01-DDL_(create_table)/01_ext_area_name.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS
+  `aesthetic-forge-293700.public_data.ext_area_name` 
+  (the_geom STRING	 OPTIONS (description = "Esto es una descripción"),
+     PERIMETER	STRING OPTIONS (description = "Esto es una descripción"),
+     AREA INT64 OPTIONS (description = "Esto es una descripción"),
+     COMAREA_ INT64 OPTIONS (description = "Esto es una descripción"),
+     COMAREA_ID INT64 OPTIONS (description = "Esto es una descripción"),
+     AREA_NUMBE INT64 OPTIONS (description = "Esto es una descripción"),
+     COMMUNITY	STRING OPTIONS (description = "Esto es una descripción"),
+     AREA_NUM_1 INT64 OPTIONS (description = "Esto es una descripción"),
+     SHAPE_AREA STRING OPTIONS (description = "Esto es una descripción"),
+     SHAPE_LEN STRING OPTIONS (description = "Esto es una descripción"));
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ 01-BigQuery/01-DDL_(create_table)/02_ext_time_in_jail.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS
+  `aesthetic-forge-293700.public_data.ext_time_in_jail` 
+  (primary_type STRING OPTIONS (description = "Tipo de Delito"),
+    days_in_jail INT64 OPTIONS (description = "Tiempo de condena en dias"));
